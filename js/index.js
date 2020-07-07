d58d8f7de6776576080f1ff71dd8905eca7da234
Bea DelRosario <bdelronyc@Beas-MacBook-Pro.local>
2020-07-06 17:50:57 -0700

    prepend

--- js/index.js
+++ js/index.js
@@ -47,15 +47,20 @@ newNavLink1.textContent = 'Blog'
 newNavLink1.href = '#'
 nav.appendChild(newNavLink1)
 
+const newNavLink2 = document.createElement('a')
+newNavLink2.textContent = 'Home'
+newNavLink2.href = '#'
+nav.prepend(newNavLink2)
+
 const navLinks = document.querySelectorAll('a')
 console.log(navLinks)
 
-navLinks[0].textContent = 'Services'
-navLinks[1].textContent = 'Product'
-navLinks[2].textContent = 'Vision'
-navLinks[3].textContent = 'Features'
-navLinks[4].textContent = 'About'
-navLinks[5].textContent = 'Contact'
+navLinks[0].textContent = siteContent.nav["nav-item-1"]
+navLinks[1].textContent = siteContent.nav["nav-item-2"]
+navLinks[2].textContent = siteContent.nav["nav-item-3"]
+navLinks[3].textContent = siteContent.nav["nav-item-4"]
+navLinks[4].textContent = siteContent.nav["nav-item-5"]
+navLinks[5].textContent = siteContent.nav["nav-item-6"]
 
 navLinks.forEach(function(item){
   item.style.color = 'green'
@@ -70,53 +75,51 @@ console.log(ctaTextBlock)
 const mainTitle = document.querySelector('h1')
 console.log(mainTitle)
 
-mainTitle.textContent = 'DOM IS AWESOME'
+mainTitle.textContent = siteContent.cta.h1
 
-mainTitle.style.fontSize = '60px'
+mainTitle.style.fontSize = '55px'
 
 const getStartedButton = document.querySelector('button')
 console.log(getStartedButton)
 
-getStartedButton.textContent = "Get Started"
+getStartedButton.textContent = siteContent.cta.button
 
 const bannerImage = document.querySelector('#cta-img')
 console.log(bannerImage)
-bannerImage.src = 'img/header-img.png'
-bannerImage.setAttribute = ('src','img/header-img.png')
+bannerImage.src = siteContent.cta["img-src"]
 
 const textContentTitle = document.querySelectorAll('h4')
 
 console.log(textContentTitle)
 
-textContentTitle[0].textContent = 'FEATURES'
-textContentTitle[1].textContent = 'ABOUT'
-textContentTitle[2].textContent = 'SERVICES'
-textContentTitle[3].textContent = 'PRODUCT'
-textContentTitle[4].textContent = 'VISION'
+textContentTitle[0].textContent = siteContent["main-content"]["features-h4"]
+textContentTitle[1].textContent = siteContent["main-content"]["about-h4"]
+textContentTitle[2].textContent = siteContent["main-content"]["services-h4"]
+textContentTitle[3].textContent = siteContent["main-content"]["product-h4"]
+textContentTitle[4].textContent = siteContent["main-content"]["vision-h4"]
 
 const textContentP = document.querySelectorAll('p')
 console.log(textContentP)
 
-textContentP[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis'
-textContentP[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
-textContentP[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
-textContentP[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
-textContentP[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+textContentP[0].textContent = siteContent["main-content"]["features-content"]
+textContentP[1].textContent = siteContent["main-content"]["about-content"]
+textContentP[2].textContent = siteContent["main-content"]["services-content"]
+textContentP[3].textContent = siteContent["main-content"]["product-content"]
+textContentP[4].textContent = siteContent["main-content"]["vision-content"]
 
 const middleImage = document.querySelector('#middle-img')
 console.log(middleImage)
 
-middleImage.src = 'img/mid-page-accent.jpg'
-middleImage.setAttribute = ('src','img/mid-page-accent.jpg')
+middleImage.src = siteContent["main-content"]["middle-img-src"]
 
 const contactSection = document.querySelector('.contact')
 console.log(contactSection)
 
-textContentTitle[5].textContent = 'CONTACT'
+textContentTitle[5].textContent = siteContent.contact["contact-h4"]
 
-textContentP[5].textContent = '123 Way 456 Street Somewhere, USA'
-textContentP[6].textContent ='1 (888) 888-8888'
-textContentP[7].textContent ='sales@greatidea.io'
-textContentP[8].textContent = 'Copyright Great Idea! 2018'
+textContentP[5].textContent = siteContent.contact.address
+textContentP[6].textContent = siteContent.contact.phone
+textContentP[7].textContent = siteContent.contact.email
+textContentP[8].textContent = siteContent.footer.copyright
 
 
